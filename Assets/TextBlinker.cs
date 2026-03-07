ddf2e59c1dc664d9628cbbff0dc634784cc8023b
seiya <seiya0417soccer@gmail.com>
2026-03-08 01:09:03 +0900

    gawa

---
+++ Assets/TextBlinker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using TMPro; // TextMeshProを使っている場合
+
+public class TextBlinker : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    public float speed = 2.0f; // 点滅スピード
+
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        // サイン波を使ってアルファ値（透明度）を0〜1で揺らす
+        float alpha = (Mathf.Sin(Time.time * speed) + 1.0f) / 2.0f;
+        text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+    }
+}
