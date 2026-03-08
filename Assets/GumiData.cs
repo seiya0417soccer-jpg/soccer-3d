02c4eab6917798f965746a58dff82fc75e828cc5
seiya <seiya0417soccer@gmail.com>
2026-03-09 04:16:17 +0900

    debug

--- Assets/GumiData.cs
+++ Assets/GumiData.cs
@@ -7,7 +7,11 @@ public class GumiData
     public int PieceCount => pieceData.Count;                    // ピース種類数
     public GumiData()
     {
+#if true
         InitPieces();
+#else
+        InitDebugPieces();
+#endif
     }
 
 
@@ -26,9 +30,22 @@ public class GumiData
         pieceData[8] = new[] { new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(0, 1), new Vector2Int(1, 1) };
         pieceData[9] = new[] { new Vector2Int(0, 0) };
     }
+    void InitDebugPieces()
+    {
+        pieceData = new Dictionary<int, Vector2Int[]>();
+        pieceData[0] = new[] { new Vector2Int(-1, 0), new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(2, 0) };
+        pieceData[1] = new[] { new Vector2Int(-1, 0), new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(2, 0) };
+        pieceData[2] = new[] { new Vector2Int(-1, 0), new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(2, 0) };
+        pieceData[3] = new[] { new Vector2Int(-1, 0), new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(2, 0) };
+        pieceData[4] = new[] { new Vector2Int(-1, 0), new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(2, 0) };
+        pieceData[5] = new[] { new Vector2Int(-1, 0), new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(2, 0) };
+        pieceData[6] = new[] { new Vector2Int(-1, 0), new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(2, 0) };
+        pieceData[7] = new[] { new Vector2Int(-1, 0), new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(2, 0) };
+        pieceData[8] = new[] { new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(0, 1), new Vector2Int(1, 1) };
+    }
 
     // --- ピースカラー ---
-    public Color[] pieceColors = 
+    public Color[] pieceColors =
     {
         Color.cyan, Color.blue, Color.green, Color.red, Color.yellow,
         Color.magenta, new Color(1f, 0.5f, 0f), new Color(0.5f, 0f, 1f),
